ba63a73c08de0cd289cc7e4f2c78c6764062c991
Jon Newton <jon.newton@gmail.com>
2017-03-18 08:41:54 -0600

    toast on top

--- GeoQuiz/app/src/main/java/com/smackwerks/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/smackwerks/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.smackwerks.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,13 +23,17 @@ public class QuizActivity extends AppCompatActivity {
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText( QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                Toast feedback = Toast.makeText( QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+                feedback.setGravity(Gravity.TOP, 0, 16);
+                feedback.show();
             }
         });
         falseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText( QuizActivity.this, R.string.wrong_toast, Toast.LENGTH_SHORT).show();
+                Toast feedback = Toast.makeText( QuizActivity.this, R.string.wrong_toast, Toast.LENGTH_SHORT);
+                feedback.setGravity(Gravity.TOP, 0, 16);
+                feedback.show();
             }
         });
     }
