2ba8c05f33d0aa2f37265fd9d3d0acbbbc98c0a8
Jon Newton <jon.newton@gmail.com>
2017-03-19 08:50:58 -0600

    added lifecycle logging

--- GeoQuiz/app/src/main/java/com/smackwerks/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/smackwerks/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.smackwerks.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    private static final String TAG = QuizActivity.class.getSimpleName();
 
     private Button trueButton;
     private Button falseButton;
@@ -30,6 +32,7 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate() called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -74,8 +77,7 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (mCurrentIndex == 0) {
                     mCurrentIndex = mQuestionBank.length - 1;
-                }
-                else {
+                } else {
                     mCurrentIndex--;
                 }
                 updateQuestion();
@@ -83,6 +85,36 @@ public class QuizActivity extends AppCompatActivity {
         });
 
         updateQuestion();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     private void updateQuestion() {
