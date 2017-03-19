340dd8b44ec81ba2d740c0ceba50bb1a31e34d76
Jon Newton <jon.newton@gmail.com>
2017-03-19 12:57:53 -0600

    added landscape, removed ImageButtons

--- GeoQuiz/app/src/main/java/com/smackwerks/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/smackwerks/geoquiz/QuizActivity.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,8 +14,7 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button trueButton;
     private Button falseButton;
-    private ImageButton mNextButton;
-    private ImageButton mPrevButton;
+    private Button mNextButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[]{
@@ -63,23 +61,11 @@ public class QuizActivity extends AppCompatActivity {
                 feedback.show();
             }
         });
-        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();
-            }
-        });
-        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
-        mPrevButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mCurrentIndex == 0) {
-                    mCurrentIndex = mQuestionBank.length - 1;
-                } else {
-                    mCurrentIndex--;
-                }
                 updateQuestion();
             }
         });
