53eb5e3eb86f06cdbb1900408e9555e5f6d79135
Jon Newton <jon.newton@gmail.com>
2017-03-26 08:48:49 -0600

    debugging bad schema uri

--- CriminalIntent/app/src/main/java/com/smackwerks/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/smackwerks/criminalintent/CrimeActivity.java
@@ -1,7 +1,12 @@
 package com.smackwerks.criminalintent;
 
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 
 public class CrimeActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+
     }
 }
