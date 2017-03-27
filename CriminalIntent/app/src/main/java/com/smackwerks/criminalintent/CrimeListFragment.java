3aecaccc5f1ca602de44dfd54ba3d3117ecb9d54
Jon Newton <jon.newton@gmail.com>
2017-03-27 16:55:47 -0600

    data binding for crimes list

--- CriminalIntent/app/src/main/java/com/smackwerks/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/smackwerks/criminalintent/CrimeListFragment.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -37,8 +38,21 @@ public class CrimeListFragment extends Fragment {
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private Crime mCrime;
+
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_crime, parent, false));
+
+            mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+        }
+
+        public void bind(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(crime.getTitle());
+            mDateTextView.setText(crime.getDate().toString());
         }
     }
 
@@ -57,6 +71,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(CrimeHolder holder, int position) {
+            holder.bind(mCrimes.get(position));
         }
 
         @Override
