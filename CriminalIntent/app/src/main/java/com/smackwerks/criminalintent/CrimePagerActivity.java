ec47f14fd12c7b0eb556083e206f18b3e88578a1
Jon Newton <jon.newton@gmail.com>
2017-04-09 17:44:12 -0600

    stub for pager activity

---
+++ CriminalIntent/app/src/main/java/com/smackwerks/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,17 @@
+package com.smackwerks.criminalintent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jwn on 4/9/2017.
+ */
+
+
+public class CrimePagerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+    }
+}
